e546f20a797415c4036c2314fbd33d65344f5a4d
abhayk <abhaykrishnan87@gmail.com>
2018-12-01 11:33:30 +0530

    Optimized day 1 solution.

--- src/solutions/Day1.java
+++ src/solutions/Day1.java
@@ -11,9 +11,7 @@ public class Day1
     public static void main(String[] args) throws IOException
     {
         List<String> input = Files.readAllLines(Paths.get("resources\\day1.txt"));
-
         System.out.println( getEffectiveSum( input ) );
-
         System.out.println( getFirstRepeatedFrequency( input ) );
     }
 
@@ -27,7 +25,7 @@ public class Day1
         {
             for( String line : input )
             {
-                sum += parseLine( line );
+                sum += Long.parseLong( line );
                 if( !freqs.add( sum ) )
                     return sum;
             }
@@ -36,16 +34,6 @@ public class Day1
 
     public static long getEffectiveSum( List<String> input )
     {
-        long sum = 0;
-        for( String item : input )
-            sum += parseLine( item );
-        return sum;
-    }
-
-    private static long parseLine(String line)
-    {
-        char op = line.charAt( 0 );
-        Long value = Long.valueOf(line.substring(1));
-        return op == '+' ? value : value * -1;
+        return input.stream().mapToLong( Long::parseLong ).sum();
     }
 }
